8a5b70be4f7900be18223443521c5420714a21ff
Ford Oxaal <ford@ipix.com>
2018-11-07 19:50:45 -0500

    solves 4 puzzles

--- app/routes/ugga.js
+++ app/routes/ugga.js
@@ -6,11 +6,10 @@ export default Route.extend({
   model() {
     let sud = {
       answer: null,
-      curPuzzleName: 'puzzle1',
+      curPuzzleName: 'puzzle3',
       blksPoss: {},
       rowsPoss: {},
       colsPoss: {},
-      solutions: {},
       puzzles: { 
 	puzzle1: {
 	  1: 'XXX15XX7X',
@@ -69,32 +68,32 @@ export default Route.extend({
 	},
       }
     };
-    return new RSVP.Promise(function(resolve) {
-      let func = function() {
-	    //let str1Row = sud.puzzles[sud.curPuzzleName][1];
-	    //for (const ch of str1) {
-	      //if (ch === 'X') {
-	      //} else {
-		//if 
-	      //}
-	//}
-	
-	/*  i r s    s      s
-	    0 1 0,3  2 0,3  3 0,3  i%3*3,i%3*3+3 
-	    1 1 3,6  2 3,6  3 3,6
-	    2 1 6,9  2 6,9  3 6,9 
-	    3 4        .
-	    4 4        .
-	    5 4        .
-	    6 7
-	    7 7
-	    8 7
-	*/
-	
-	// Input arr, output arr of numbers between 1-9 not in arr
-	let eliminate = function(str) {
+    return new RSVP.Promise((resolve) => {
+      let func = () => {
+	// Input any number of strings containing [1-9|X] and find single
+	// occurences.  Like a histogram.
+	let uniques = function(...strs) {
+	  let str = '';
+	  let acc = [0,0,0,0,0,0,0,0,0];
+	  for (let str of strs) {
+	    for (let chr of str) {
+	      if (chr !== 'X') {
+		let val = Number(chr);
+		acc[val-1]++;
+	      }
+	    }
+	  }
+	  acc.forEach(function(val, idx) {
+	    if (val === 3) str = str + String(idx+1);
+	  })
+	  //console.log('uniques:', str);
+	  return str;
+	};
+
+	// Input str, output str of numerals between 1-9 not in str
+	let eliminate = (str) => {
 	  let ret = '';
-	  // Iterate over possible numerals in str
+	  // Iterate over possible values in str which are numerals 1-9
 	  for (let i = 1; i <= 9; i++) {
 	    // Stringify i. Note, this is diff from *new* String(i)!
 	    let istr = String(i);
@@ -104,164 +103,164 @@ export default Route.extend({
 	  // Return numerals not in str
 	  return ret;
 	};
-	let getCol = function(idx) {
+
+	let getCol = (idx) => {
 	  if (idx < 0 || idx > 8) {
 	    console.log('getRow: ERROR: idx not within 0-8', idx);
 	    return;
 	  }
 	  let ret = null;
-	  for (let i = 0; i < 9; i++) {
-	    let row = sud.puzzles[sud.curPuzzleName][i+1];
+	  for (let j = 0; j < 9; j++) {
+	    let row = sud.puzzles[sud.curPuzzleName][j+1];
 	    ret = ret + row[idx];
 	  }
 	  return ret;
 	};
-	let getRow = function(idx) {
+
+	let getRow = (idx) => {
 	  if (idx < 1 || idx > 9) {
 	    console.log('getRow: ERROR: idx not within 1-9', idx);
 	    return;
 	  }
 	  return sud.puzzles[sud.curPuzzleName][idx];
 	};
-	let getBlksPoss = function() {
+
+	let copyPuzzleToSolution = (puzzleName) => {
+	  let solveName = puzzleName.replace('puzzle', 'solve');
+	  let sol = {};
+	  for (let j = 0; j < 9; j++) {
+	    let row = sud.puzzles[sud.curPuzzleName][j+1];
+	    sol[j+1] = row;
+	  }
+	  sud.puzzles[solveName] = sol;
+	  sud.curPuzzleName = solveName;
+	};
+
+	// i is across, j is down
+	let traverseEmptyCells = (func) => {
+	  let count = 0, xcount = 0, curcount = 0;
+	  for (let j = 0; j < 9; j++) {
+	    let row = sud.puzzles[sud.curPuzzleName][j+1];
+	    for (let i = 0; i < 9; i++) {
+	      if (row[i] === 'X') {
+		xcount++;
+		curcount = func(i,j);
+		if (curcount) {
+		  count++;
+		  break;
+		}
+	      }
+	    }
+	  }
+	  return {count, xcount, curcount};
+	};
+
+	/*  i  row | s1,s2 (string indices for getting substring)
+	    -------------------------------------------------------------
+	    0    1 | 0,3  2 | 0,3  3 | 0,3     
+	    1    1 | 3,6  2 | 3,6  3 | 3,6
+	    2    1 | 6,9  2 | 6,9  3 | 6,9 
+	    3    4             .
+	    4    4             .
+	    5    4             .
+	    6    7
+	    7    7
+	    8    7
+	    -------------------------------------------------------------
+	    row-frmla: Math.floor(i/3)*3+1  str-idx-frmla: i%3*3, i%3*3+3 */
+	let getBlksPoss = () => {
 	  let ret = 'blank';
 	  for (let i = 0; i < 9; i++) {
 	    let r,s1,s2;
 	    s1 = (i%3)*3;
 	    s2 = s1+3;
-	    if (i < 3) {
-	      r = 1;
-	    } else if (i < 6) {
-	      r = 4;
-	    } else {
-	      r = 7;
-	    }
+	    r = Math.floor(i/3)*3+1;
 	    let str1 = sud.puzzles[sud.curPuzzleName][r].substring(s1,s2);
 	    let str2 = sud.puzzles[sud.curPuzzleName][r+1].substring(s1,s2);
 	    let str3 = sud.puzzles[sud.curPuzzleName][r+2].substring(s1,s2);
 	    sud.blksPoss[i+1] = eliminate(str1+str2+str3); 
 	  }
 	};
-	let getRowsPoss = function() {
-	  for (let i = 0; i < 9; i++) {
-	    let row = sud.puzzles[sud.curPuzzleName][i+1];
-	    sud.rowsPoss[i+1] = eliminate(row);
+
+	let getRowsPoss = () => {
+	  for (let j = 0; j < 9; j++) {
+	    let row = sud.puzzles[sud.curPuzzleName][j+1];
+	    sud.rowsPoss[j+1] = eliminate(row);
 	  }
 	};
-	let getColsPoss = function() {
+
+	let getColsPoss = () => {
 	  for (let i = 0; i < 9; i++) {
 	    sud.colsPoss[i+1] = eliminate(getCol(i));
 	  }
 	};
-	let traverseEmptyCells = function(funkyfunc) {
-	  let count = 0;
-	  for (let i = 0; i < 9; i++) {
-	    let row = sud.puzzles[sud.curPuzzleName][i+1];
-	    for (let j = 0; j < 9; j++) {
-	      if (row[j] === 'X') {
-		count = count + funkyfunc(i,j);
-	      }
-	    }
-	  }
-	  return count;
+
+	let setCell = (i,j,chr) => {
+	  let row = getRow(j+1);
+	  //console.log('setCell bef:', row);	  
+	  let newStr = row.substr(0,i) + chr + row.substr(i+1);
+	  sud.puzzles[sud.curPuzzleName][j+1] = newStr;
+	  //console.log('setCell aft:', getRow(j+1));	  
 	};
-	let setCell = function(i,j,chr) {
-	  let row = getRow(i+1);
-	  return row.substr(0,j) + chr + row.substr(j+1);
+
+	let getCell = (i,j) => {
+	  let row = getRow(j+1);
+	  let val = row[i];
+	  return val;
 	};
-	let setSolvedCell = function(i,j) {
-	  let rowPoss = sud.rowsPoss[i+1];
-	  let colPoss = sud.colsPoss[j+1];
-	  let blkPoss = sud.blksPoss[(i%3)*(j%3)+1];
-	  let cellPoss = eliminate(rowPoss+colPoss+blkPoss);
-	  if (cellPoss.length === 1) {
-	    setCell(i, j, cellPoss);
+
+	let setSolvedCell = (i,j) => {
+	  let rowPoss = sud.rowsPoss[j+1];
+	  let colPoss = sud.colsPoss[i+1];
+	  let blkIdx = Math.floor(i/3) + (Math.floor(j/3)*3) + 1; 
+	  let blkPoss = sud.blksPoss[blkIdx];
+	  let totalPoss = uniques(rowPoss, colPoss, blkPoss);
+	  if (totalPoss.length === 1) {
+	    console.log('Solved:'
+			, i+1, j+1, 'blkidx:', blkIdx, 'val:', getCell(i,j)
+			,'\n  rowPoss', rowPoss
+			,'\n  colPoss', colPoss
+			,'\n  blkPoss', blkPoss
+			,'\n  totalPoss', totalPoss);
+	    setCell(i, j, totalPoss);
 	    return 1;
 	  }
 	  return 0;
 	};
-	let loadPossBlkRowCol = function() {
+	/*
+	let checkSolution = () => {
+	  ret = true;
+	  let sol = sud.puzzles[sud.curPuzzleName];
+	  // check for X
+	  
+	  return ret;
+	};
+	*/
+	let loadPossBlkRowCol = () => {
 	  getColsPoss();
 	  getRowsPoss();
 	  getBlksPoss();
 	  let count = traverseEmptyCells(setSolvedCell);
-	  if (count)
-	    loadPossBlkRowCol();
+	  if (count.count) loadPossBlkRowCol();
+	  sud.answer = count.count+'-'+count.xcount+'-'+count.curcount;
+	  console.log('loadPoss', sud);
+	  //let good = checkSolution();
+	  //if (!good) brutish();
 	};
+
+	copyPuzzleToSolution(sud.curPuzzleName);
 	loadPossBlkRowCol();
 
-	//console.log('func:', str);
-	sud.answer = sud.answer+'-'+str;
 	resolve(sud);
       };
       //setTimeout(func, 3000);
       func();
     });
   },
-  /*
-  model() {
-    return {
-      puzzle1: {
-	a: 'XXX15XX7X',
-	b: '1X6XXX82X',
-	c: '3XX86XX4X',
-	d: '9XX4XX567',
-	e: 'XX47X83XX',
-	f: '732XX6XX4',
-	g: 'X4XX81XX9',
-	h: 'X17XXX2X8',
-	i: 'X5XX37XXX',
-      },
-      puzzle2: {
-	a: 'X21XXX543',
-	b: 'X6X51X872',
-	c: '87X4X2X61',
-	d: 'XX4283X1X',
-	e: 'XXX6453X9',
-	f: '6X8971XX5',
-	g: '3X9X271X6',
-	h: '25X194738',
-	i: '1XXX5XX9X',
-      },
-      puzzle3: {
-	a: 'XXX7X8XXX',
-	b: 'X6XXX98XX',
-	c: 'XXX43XXXX',
-	d: 'XXXXXX6XX',
-	e: 'X1XXX5X89',
-	f: 'X38XXX4XX',
-	g: '34X8X71X6',
-	h: 'X56X9X73X',
-	i: '1XXX5XX9X',
-      },
-      puzzle4: {
-	a: '53XX7XXXX',
-	b: '6XX195XXX',
-	c: 'X98XXXX6X',
-	d: '8XXX6XXX3',
-	e: '4XX8X3XX1',
-	f: '7XXX2XXX6',
-	g: 'X6XXXX28X',
-	h: 'XXX419XX5',
-	i: 'XXXX8XX79',
-      },
-      puzzle5: {
-	a: 'XXXXXXXX7',
-	b: 'X486X2159',
-	c: 'X6XXXXX8X',
-	d: 'X9X8X4X3X',
-	e: 'XXXX2XXXX',
-	f: 'X2X9X5X6X',
-	g: 'X7XXXXX9X',
-	h: '1832X754X',
-	i: '2XXXXXXXX',
-      },
-    }
-  },
-  */
+
+
   setupController(controller, model) {
     this._super(...arguments);
     controller.set('sud', model);
-    //controller.solveGames();
   },
 });
